98d43cc2826d834c0439a838388ac787266ad314
Peter Christian Würdemann <christian.wuerdemann@gmail.com>
2025-06-25 17:14:58 +0200

    balkendiagramme in monitoring

---
+++ apps/client/src/components/diagnostik/Diagramme/SchuelerResultBalkenDiagramm.tsx
@@ -0,0 +1,64 @@
+import { Tooltip } from "@/components/Tooltip"
+import type { Ergebnis } from "@thesis/diagnostik"
+
+interface SchuelerResultBalkenDiagrammProps {
+    obereGrenze: number,
+    untereGrenze: number,
+    mindeststandard: number,
+    ergebnis: number,
+    label?: string
+}
+export const SchuelerResultBalkenDiagramm = (props: SchuelerResultBalkenDiagrammProps) => {
+
+    const { obereGrenze, untereGrenze, mindeststandard, ergebnis, label = '2025-02-28'} = props;
+    // const obereGrenze = 100
+    // const untereGrenze = 0
+    // const mindestStandard = 50
+    // const sample: Ergebnis = {
+    //     datum: '2025-02-25',
+    //     ergebnis: '80',
+    //     schuelerId: -1
+    // } 
+
+    const widthInPx = 50
+    const heightInPx = 250
+
+    const heightGreenDivInPercent = 1 - ((mindeststandard - untereGrenze) / (obereGrenze - untereGrenze))
+    const heightResultInPercent = (ergebnis - untereGrenze) / (obereGrenze - untereGrenze)
+    const heightResultBarInPx = 2
+    return <div className="flex flex-col items-center min-w-[100px]">
+        <Tooltip content={`Ergebnis: ${ergebnis} Mindeststandard: ${mindeststandard}`}>
+            <div style={{ 
+                height: `${heightInPx}px`,
+                width: `${widthInPx}px`
+            }} 
+            className={`flex flex-col relative`}>
+                <div style={{ 
+                    minHeight: `${heightGreenDivInPercent * 100}%`,
+                    width: `${widthInPx /2}px`,
+                    marginLeft: `${Math.floor(widthInPx /4)}px`
+                }} className="bg-green-500">
+                    
+                </div>
+
+                <div style={{ 
+                    minHeight: `${(1-heightGreenDivInPercent) * 100}%`,
+                    width: `${widthInPx /2}px`,
+                    marginLeft: `${Math.floor(widthInPx /4)}px`
+                }} className="bg-red-400">
+                </div>
+
+                <div
+                    className="absolute bg-black w-full"
+                    style={{
+                        top: `${heightInPx * (1 - heightResultInPercent) - heightResultBarInPx / 2}px`,
+                        height: `${heightResultBarInPx}px`,
+                    }}
+                ></div>
+            </div>
+        </Tooltip>
+        <label>
+            {label}
+        </label>
+    </div>
+}
--- apps/client/src/routes/(app)/schueler/$schuelerId/monitoring.tsx
+++ apps/client/src/routes/(app)/schueler/$schuelerId/monitoring.tsx
@@ -1,6 +1,8 @@
+import { SchuelerResultBalkenDiagramm } from '@/components/diagnostik/Diagramme/SchuelerResultBalkenDiagramm';
 import { useDiagnostikSchuelerData } from '@/components/schueler/useDiagnostikSchuelerData';
 import { SchuelerNav } from '@/layout/SchuelerNav';
 import { createFileRoute } from '@tanstack/react-router'
+import { getMindeststandard } from '@thesis/diagnostik';
 
 export const Route = createFileRoute('/(app)/schueler/$schuelerId/monitoring')({
   component: RouteComponent,
@@ -8,16 +10,54 @@ export const Route = createFileRoute('/(app)/schueler/$schuelerId/monitoring')({
 
 function RouteComponent() {
   const { schuelerId } = Route.useParams();
-
-  const { query, invalidate } = useDiagnostikSchuelerData(schuelerId);
+  
+  const { query } = useDiagnostikSchuelerData(schuelerId);
 
   if (query.isPending) {
     return;
   }
 
-  console.log(query.data)
+  if (!query.data || !query.data.data) {
+    return;
+  }
+  const diagnostiken = query.data.data
   
   return <div className='flex flex-col w-full'>
     <SchuelerNav schuelerId={schuelerId} />
+
+    <div className='p-2 xl:p-8'>
+        <ul className='flex flex-col gap-8 w-full'>
+          {
+            diagnostiken.map(diagnostik => {
+              return <li className='border-black border-[1px] rounded-xl p-4 flex flex-col gap-4'>
+
+                  <h2>Diagnostik: {diagnostik.name}</h2>
+
+                  <div className='flex gap-2'>
+                    {
+                      diagnostik.ergebnisse && diagnostik.ergebnisse.map(row => {
+                        return <>
+                          {
+                            row.ergebnisse.map(ergebnis => {
+                              return <SchuelerResultBalkenDiagramm  
+                                obereGrenze={parseInt(`${diagnostik.obereGrenze ?? -1}`)}
+                                untereGrenze={parseInt(`${diagnostik.untereGrenze ?? -1}`)}
+                                mindeststandard={getMindeststandard(diagnostik) ?? -1}  
+                                ergebnis={parseInt(ergebnis.ergebnis)}
+                                label={new Date(ergebnis.datum ?? '').toLocaleDateString('de')}   
+                              />
+                            })
+                          }
+                        </>
+                      }) 
+                    }
+                  </div>
+              </li>
+            })
+          }
+        </ul>
+    </div>
+
+    
   </div>
 }
