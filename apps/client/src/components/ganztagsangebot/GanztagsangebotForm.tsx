db10f19275e3058f0fce2a12fa371a213cbdbac5
Peter Christian Würdemann <christian.wuerdemann@gmail.com>
2025-06-25 12:40:38 +0200

    fixed ganztagsangebot creation

--- apps/client/src/components/ganztagsangebot/GanztagsangebotForm.tsx
+++ apps/client/src/components/ganztagsangebot/GanztagsangebotForm.tsx
@@ -1,9 +1,12 @@
-import type { Dispatch, SetStateAction } from "react";
+import { useState, type Dispatch, type SetStateAction } from "react";
 import { ButtonLight } from "../ButtonLight"
 import { Input } from "../Input";
-import { SchuelerSelectionList } from "../schueler/SchuelerSelectionList";
 import { Berechtigung } from "@thesis/rollen";
 import { SelectedUserCtrl } from "../shared/SelectedUserCtrl";
+import { Autocomplete } from "../shared/Autocomplete";
+import { Trash2 } from "lucide-react";
+import { useSchuelerStore } from "../schueler/SchuelerStore";
+import { useAllSchueler } from "../schueler/useSchueler";
 
 interface GanztagsangebotFormProps {
   onAbort: () => void,
@@ -20,9 +23,49 @@ interface GanztagsangebotFormProps {
 }
 
 export const GanztagsangebotForm = (props: GanztagsangebotFormProps) => {
-    const { title, submitButtonText, onAbort, onSubmit, cancelButtonClassName, submitButtonClassName } = props;
+    const { setSelectedSchueler, selectedSchueler, title, submitButtonText, onAbort, onSubmit, cancelButtonClassName, submitButtonClassName } = props;
 
+    const schuelerData = useSchuelerStore(store => store.schueler)
+    const [queryResults, setQueryResults] = useState([] as number[])
+    useAllSchueler()
+    const [query, setQuery] = useState('')
+    function handleQueryChange(newVal: string) {
+        
+        const filtered = schuelerData
+            .filter(schueler => `${schueler.vorname} ${schueler.nachname}`.includes(newVal))  
 
+        setQueryResults(
+            filtered.map(item => item.id ?? -1)
+        )
+
+        setQuery(newVal)
+    }
+    function handleSchuelerSelection(idx: number, selected: number) {
+        setSelectedSchueler(selectedSchueler.map((item, i) => {
+            if (i !== idx) {
+                return item
+            }
+            return selected
+        }))
+    }
+    function getLabel(val: any) {
+        const found = schuelerData.find(item => item.id == val)
+        if (val == -1) {
+            return 'Kein Schüler wurde ausgewählt.'
+        }
+        if (!found) {
+            return 'Ein Fehler ist aufgetreten.'
+        }
+        return `${found.vorname} ${found.nachname}`
+    }
+    function removeSchueler(idx: number) {
+        setSelectedSchueler(prev => prev.filter((_, i) => {
+                if (i !== idx) return true;
+                return false;
+            })
+        )
+    }
+ 
     return <div className="h-[60vh]">
         <h1>{title ? title : 'Ganztagsangebot hinzufügen'}</h1>
 
@@ -35,23 +78,43 @@ export const GanztagsangebotForm = (props: GanztagsangebotFormProps) => {
         </div>
 
         <SelectedUserCtrl
-            berechtigung={Berechtigung.GanztagsangebotCreate as Berechtigung} 
+            berechtigung={Berechtigung.GanztagsangebotRead as Berechtigung} 
             berechtigungValue={["alle", "eigene"]} 
             placeholder="Namen eingeben"
             label="Betreuer"
         />
 
-        <SchuelerSelectionList selectedIds={props.selectedSchueler}  handleSelection={(element) => {
-            if (props.selectedSchueler.includes(element.id ?? -1)) {
-                props.setSelectedSchueler(prev => {
-                    return prev.filter(item => item !== element.id)
-                }) 
-            } else {
-                props.setSelectedSchueler(prev => [...prev, element.id ?? -1]) 
-            }
-        }}/>
-        
+        <label>
+            Schüler
+        </label>
+        <div className="flex flex-col gap-2">
 
+        
+        {
+            props.selectedSchueler.map((schueler, idx) => {
+                return <div className="flex gap-2">
+                    <Autocomplete 
+                        key={idx}
+                        query={query}
+                        setQuery={handleQueryChange}
+                        selected={schueler}
+                        setSelected={(selected: any) => handleSchuelerSelection(idx, selected)}
+                        getLabel={getLabel}
+                        placeholder="Max Mustermann"
+                        queryResults={queryResults}
+                    />
+                    <button onClick={() => removeSchueler(idx)}>
+                        <Trash2 />
+                    </button>
+                </div>
+            })
+        }
+        <ButtonLight onClick={() => {
+            setSelectedSchueler([...selectedSchueler, -1])
+        }}>
+            Schüler hinzufügen
+        </ButtonLight> 
+        </div>       
 
          <div className="flex gap-2 py-8">
             <ButtonLight className={cancelButtonClassName} onClick={() => onAbort()}>
