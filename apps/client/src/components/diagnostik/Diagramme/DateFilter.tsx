72f3968e2e80930c241ac65fc2b5e85a6a348c83
Peter Christian Würdemann <christian.wuerdemann@gmail.com>
2025-06-28 23:23:16 +0200

    min and max datum order fix

--- apps/client/src/components/diagnostik/Diagramme/DateFilter.tsx
+++ apps/client/src/components/diagnostik/Diagramme/DateFilter.tsx
@@ -10,25 +10,26 @@ export const DateFilter = (props: DateFilterProps) => {
     const { minDate, setMinDate, maxDate, setMaxDate } = props
 
     return <div className="flex flex-col">
-        <label>
-            Max. datum:
-        </label>
-        <Input type='date' value={minDate} onChange={(e) => {
-            const newVal = e.target.value
-            if (newVal !== undefined && maxDate !== undefined && new Date(newVal) > new Date(maxDate)) {
-                return alert('Das Datum darf nicht größer sein als max. Datum.')
-            } 
-            setMinDate(e.target.value)}
-        } />
+        
         <label>
             Min. datum:
         </label>
-        <Input type='date' value={maxDate} onChange={(e) => {
+        <Input type='date' value={minDate} onChange={(e) => {
             const newVal = e.target.value
             if (newVal !== undefined && minDate !== undefined && new Date(newVal) < new Date(minDate)) {
                 return alert('Das Datum darf nicht kleiner sein als min. Datum.')
             } 
-            setMaxDate(e.target.value)
+            setMinDate(e.target.value)
         }} />
+        <label>
+            Max. datum:
+        </label>
+        <Input type='date' value={maxDate} onChange={(e) => {
+            const newVal = e.target.value
+            if (newVal !== undefined && maxDate !== undefined && new Date(newVal) > new Date(maxDate)) {
+                return alert('Das Datum darf nicht größer sein als max. Datum.')
+            } 
+            setMaxDate(e.target.value)}
+        } />
     </div>
 }
